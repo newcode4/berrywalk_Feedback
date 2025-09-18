79ec7d040d515715d39ba3474c472aaca1ef57e7
Sloa <past1303@gmail.com>
2025-09-18 12:52:42 +0900

    수정

--- berrywalk-feedback-acf/berrywalk-feedback-acf.php
+++ berrywalk-feedback-acf/berrywalk-feedback-acf.php
@@ -11,6 +11,23 @@ define('BWOS_VER', '1.2.2');
 define('BWOS_DIR', plugin_dir_path(__FILE__));
 define('BWOS_URL', plugin_dir_url(__FILE__));
 
+// GitHub 업데이트 확인 (admin에서만 동작)
+if ( is_admin() ) {
+  require_once BWOS_DIR.'lib/plugin-update-checker/plugin-update-checker.php';
+  $bwos_uc = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/newcode4/berrywalk_Feedback', // 깃허브 저장소
+    __FILE__,                                         // 이 플러그인 메인 파일
+    'bw-owner-suite-acf'                              // 플러그인 슬러그(폴더명)
+  );
+  $bwos_uc->setBranch('main'); // 기본 브랜치가 main일 때
+  // 릴리스 Asset(zip) 사용 시:
+  $api = $bwos_uc->getVcsApi();
+  if (method_exists($api, 'enableReleaseAssets')) {
+    $api->enableReleaseAssets();
+  }
+}
+
+
 // 텔레그램
 define('BW_TG_BOT_TOKEN', '8060380419:AAFDlmr9TmX1K5Ocagl6WafN5s2O6oVnvdw');  // BotFather에서 발급
 define('BW_TG_CHAT_ID',  '@berrywalk_ownerfeedback');                  // 개인(@me) 또는 채널/그룹 ID
